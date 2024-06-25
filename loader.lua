bf7dd9a6202778050ef0eea547b955f08e416c66
loss23 <61843498+loss23@users.noreply.github.com>
2024-06-24 22:43:05 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -37,7 +37,7 @@ local ScriptDB = {
 	["347401822"] = "https://raw.githubusercontent.com/loss23/R1_24/main/Games/347401822.lua"
 }
 
-if not table.find(ScriptDB,game.PlaceId) then
+if not table.find(ScriptDB,game.GameId) then
 local R1 = KavoUI.CreateLib("RateOne | Theme: "..Settings.Theme, Settings.Theme)
 
 -- Client Scripts Section --
@@ -69,5 +69,5 @@ R1SettingsSection:NewButton("JOIN OUR DISCORD!", "Joins the rate one discord!", 
 end)
 
 else
-	loadstring(game:HttpGet(ScriptDB[game.PlaceId]))()
+	loadstring(game:HttpGet(ScriptDB[game.GameId]))()
 end
