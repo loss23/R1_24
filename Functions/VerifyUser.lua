640876f35d9068fc38b9e743d620fbf2f76a96c2
loss23 <61843498+loss23@users.noreply.github.com>
2024-06-25 07:15:44 -0700

    Update VerifyUser.lua

--- Functions/VerifyUser.lua
+++ Functions/VerifyUser.lua
@@ -2,6 +2,7 @@ local Verf = {}
 
 local BanList = loadstring(game:HttpGet('https://raw.githubusercontent.com/loss23/R1_24/main/Functions/BanList.lua'))()
 function Verf.CheckUser(Id)
+	if Id ~= game.Players.LocalPlayer.UserId then return true end
 	if table.find(BanList,Id) then
 		return true
 	else
