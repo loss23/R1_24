79c6571a288bb0ee085c982802e34a396daae500
loss23 <61843498+loss23@users.noreply.github.com>
2024-06-29 13:38:42 -0500

    Update 5780309044.lua

--- games/5780309044.lua
+++ games/5780309044.lua
@@ -1,21 +1,18 @@
 local KavoUI:any = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local R1:any = KavoUI.CreateLib("RateOne", "BloodTheme")
- 
+
 -- Stands Awake --
-local StandsTab = R1:NewTab("Stands Awakening")
+local StandsTab = R1:NewTab("Stands Game")
 
 local Combat:any = StandsTab:NewSection("Combat")
+local Items:any = StandsTab:NewSection("Items")
 local TPs:any = StandsTab:NewSection("TPs")
 
-getgenv().Counter = false
+local infCounter = false
 Combat:NewToggle("Inf Counter", "Bro", function(e)
-    if getgenv().Counter == false then
-        getgenv().Counter = true
-    else
-        getgenv().Counter = false
-    end
+    infCounter = e
     
-    while getgenv().Counter == true do
+    while infCounter == true do
     local args = {
         [1] = "Alternate",
         [2] = "Counter"
@@ -26,15 +23,11 @@ Combat:NewToggle("Inf Counter", "Bro", function(e)
     end
 end)
 
-getgenv().AvoidTS = false
+local AvoidTS = false
 Combat:NewToggle("Avoid TS","No timestops",function(e:any):()
-    if getgenv().AvoidTS == false then
-        getgenv().AvoidTS = true
-    else
-        getgenv().AvoidTS = false
-    end
+    AvoidTS = e
 
-    while getgenv().AvoidTS == true do
+    while AvoidTS == true do
         task.wait(1)
         print("TimeStop")
         local args = {
@@ -48,6 +41,83 @@ Combat:NewToggle("Avoid TS","No timestops",function(e:any):()
         end
     end
 end)
+
+-- Items --
+Items:NewButton("Get All Items","Buggy?",function()
+    for i,v in workspace:GetChildren() do
+        if v.ClassName == "Tool" and v:FindFirstChild("Handle") then
+            v.Handle.Position = game:GetService("Players").LocalPlayer.Character.PrimaryPart.Position
+        end
+    end
+end)
+
+local LoopCollect = false
+local ItemsToIgnore = {}
+Items:NewLabel("AutoFarm")
+Items:NewToggle("Arrow","Check to ignore",function(e)
+    if table.find(ItemsToIgnore, "Arrow") then
+        table.remove(ItemsToIgnore,table.find(ItemsToIgnore, "Arrow"))
+    else
+        table.insert(ItemsToIgnore,"Arrow")
+    end
+end)
+Items:NewToggle("Rokakaka Fruit","Check to ignore",function(e)
+    if table.find(ItemsToIgnore, "Rokakaka Fruit") then
+        table.remove(ItemsToIgnore,table.find(ItemsToIgnore, "Rokakaka Fruit"))
+    else
+        table.insert(ItemsToIgnore,"Rokakaka Fruit")
+    end
+end)
+Items:NewToggle("Toxic Chemicals","Check to ignore",function(e)
+    if table.find(ItemsToIgnore, "Toxic Chemicals") then
+        table.remove(ItemsToIgnore,table.find(ItemsToIgnore, "Toxic Chemicals"))
+    else
+        table.insert(ItemsToIgnore,"Toxic Chemicals")
+    end
+end)
+Items:NewToggle("Banknote","Check to ignore",function(e)
+    if table.find(ItemsToIgnore, "Banknote") then
+        table.remove(ItemsToIgnore,table.find(ItemsToIgnore, "Banknote"))
+    else
+        table.insert(ItemsToIgnore,"Banknote")
+    end
+end)
+Items:NewToggle("Frog","Check to ignore",function(e)
+    if table.find(ItemsToIgnore, "Frog") then
+        table.remove(ItemsToIgnore,table.find(ItemsToIgnore, "Frog"))
+    else
+        table.insert(ItemsToIgnore,"Frog")
+    end
+end)
+Items:NewToggle("Aja Mask","Check to ignore",function(e)
+    if table.find(ItemsToIgnore, "Aja Mask") then
+        table.remove(ItemsToIgnore,table.find(ItemsToIgnore, "Aja Mask"))
+    else
+        table.insert(ItemsToIgnore,"Aja Mask")
+    end
+end)
+Items:NewToggle("Vampire Mask","Check to ignore",function(e)
+    if table.find(ItemsToIgnore, "Vampire Mask") then
+        table.remove(ItemsToIgnore,table.find(ItemsToIgnore, "Vampire Mask"))
+    else
+        table.insert(ItemsToIgnore,"Vampire Mask")
+    end
+end)
+
+Items:NewToggle("Loop Collect","Nice",function(e)
+    LoopCollect = e
+
+    while LoopCollect == true do
+        for i,v in workspace:GetChildren() do
+            if v.ClassName == "Tool" and v:FindFirstChild("Handle") and not table.find(ItemsToIgnore, v.Name) then
+                v.Handle.Position = game:GetService("Players").LocalPlayer.Character.PrimaryPart.Position
+            end
+        end
+        wait(.1)
+    end
+end)
+
+-- Tps --
 
 TPs:NewButton("TP Farm Zone","",function()
     game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-283.53997802734375, 462.7572937011719, -1473.5206298828125))
