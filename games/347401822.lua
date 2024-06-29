f1f42016f095bd39c622e625e9113dddb1b8679c
loss23 <61843498+loss23@users.noreply.github.com>
2024-06-29 12:44:20 -0500

    The Plaza

--- games/347401822.lua
+++ games/347401822.lua
@@ -1,6 +1,6 @@
 local KavoUI:any = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local R1:any = KavoUI.CreateLib("RateOne", "BloodTheme")
-
+ 
 -- Plaza Script Section --
 local PlazaTab = R1:NewTab("Plaza Scripts")
 
