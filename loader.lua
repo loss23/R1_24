effaf3cb63b289ea690f7aae0515f012147d8d92
loss23 <mebob248888@gmail.com>
2024-06-28 21:32:53 -0500

    T

---
+++ loader.lua
@@ -0,0 +1,4 @@
+local KavoUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local R1 = Library.CreateLib("RateOne", "BloodTheme")
+
+loadstring(game:HttpGet(''))()
