930764cdd4391766bc1246340d4f482e5efdbfc9
loss23 <61843498+loss23@users.noreply.github.com>
2024-06-29 20:15:20 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -5,6 +5,7 @@ local ClientSettings = {
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/loss23/R1_24/main/Functions/key.lua'))()
 R1KEY = _G.R1KEY
+DevKey = _G.DevKey
 
 if tostring(ClientSettings.Key) ~= tostring(R1KEY) then 
     game.StarterGui:SetCore("SendNotification",  {
@@ -19,31 +20,37 @@ end
 local KavoUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local R1 = KavoUI.CreateLib("RateOne", ClientSettings.Theme)
 
-local R1Tab = R1:NewTab("RateOne")
-local R1SettingsSection = R1Tab:NewSection("Settings",true)
-
-function LoadClientScript(ScriptName)
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/loss23/R1_24/main/ClientScripts/'..ScriptName..'.lua'))()
+if tostring(ClientSettings.Key) ~= tostring(DevKey) then 
+	local R1Tab = R1:NewTab("RateOne")
+	local R1SettingsSection = R1Tab:NewSection("Settings",true)
+	
+	function LoadClientScript(ScriptName)
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/loss23/R1_24/main/ClientScripts/'..ScriptName..'.lua'))()
+	end
+	
+	-- Client Scripts Section --
+	local ClientTab = R1:NewTab("Client Scripts")
+	local ClientSection = ClientTab:NewSection("Client Scripts",false)
+	
+	ClientSection:NewButton("Fling Script","Loads fling script",function()
+		LoadClientScript("fling")
+	end)
+	
+	R1SettingsSection:NewLabel("Credits: thefrx0x & loser5808")
+	R1SettingsSection:NewButton("JOIN OUR DISCORD!", "Joins the rate one discord!", function()
+		setclipboard("https://discord.gg/tyKb9KXm")
+		game.StarterGui:SetCore("SendNotification",  {
+			Title = "▀▄『R』『1』▄▀";
+			Text = "Invite copied";
+			Icon = "rbxassetid://10223592622";
+			Duration = 4.5;
+		})
+	end)
+else
+	local R1SettingsSection = R1Tab:NewSection("Settings",true)
+	R1SettingsSection:NewButton("Inf Yield","All ur gonna need",function()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+	end)
 end
-
--- Client Scripts Section --
-local ClientTab = R1:NewTab("Client Scripts")
-local ClientSection = ClientTab:NewSection("Client Scripts",false)
-
-ClientSection:NewButton("Fling Script","Loads fling script",function()
-	LoadClientScript("fling")
-end)
-
-R1SettingsSection:NewLabel("Credits: thefrx0x & loser5808")
-R1SettingsSection:NewButton("JOIN OUR DISCORD!", "Joins the rate one discord!", function()
-	setclipboard("https://discord.gg/tyKb9KXm")
-	game.StarterGui:SetCore("SendNotification",  {
-		Title = "▀▄『R』『1』▄▀";
-		Text = "Invite copied";
-		Icon = "rbxassetid://10223592622";
-		Duration = 4.5;
-	})
-end)
-
 --Load Game Script Hub
 loadstring(game:HttpGet('https://raw.githubusercontent.com/loss23/R1_24/main/games/'..game.PlaceId..'.lua'))()
